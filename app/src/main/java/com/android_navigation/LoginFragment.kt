56f8859a58d31e0741dce123d7bab11f90953f07
Gustavo <pereiragustavo215@gmail.com>
2020-04-20 10:02:08 -0300

    criando tela login

---
+++ app/src/main/java/com/android_navigation/LoginFragment.kt
@@ -0,0 +1,33 @@
+package com.android_navigation
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.login.*
+
+class LoginFragment : Fragment() {
+
+    private val controller by lazy {
+        findNavController()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        login_botao_logar.setOnClickListener {
+            val diresion = LoginFragmentDirections.actionLoginToListaProdutos()
+            controller.navigate(diresion)
+        }
+    }
+}
