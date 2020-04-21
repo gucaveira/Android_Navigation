b04adcbf96b60461ff24e955a3918996f17d3422
Gustavo <pereiragustavo215@gmail.com>
2020-04-21 19:56:11 -0300

    Aplicando configuracoes na mudanca de destinos

--- app/src/main/java/com/android_navigation/ui/activity/ProdutoActivity.kt
+++ app/src/main/java/com/android_navigation/ui/activity/ProdutoActivity.kt
@@ -2,12 +2,24 @@ package com.android_navigation.ui.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 import com.android_navigation.R
 
 class MainActivity : AppCompatActivity() {
 
+    private val controller by lazy {
+        findNavController(R.id.main_activity_nav_host)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+        controller.addOnDestinationChangedListener { controller, destination, arguments ->
+            title = destination.label
+            when (destination.id) {
+                R.id.listaProdutos -> supportActionBar?.show()
+                R.id.Login -> supportActionBar?.hide()
+            }
+        }
     }
 }
