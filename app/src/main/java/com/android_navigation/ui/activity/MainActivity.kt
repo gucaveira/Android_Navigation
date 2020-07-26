9746902ff6274e697c6c0cccbca0af35704d5046
gustavo <pereiragustavo215@gmail.com>
2020-07-26 12:33:28 -0300

    add projeto no firebase e criando user de teste

--- app/src/main/java/com/android_navigation/ui/activity/MainActivity.kt
+++ app/src/main/java/com/android_navigation/ui/activity/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.android_navigation.ui.activity
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View.GONE
 import android.view.View.VISIBLE
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.android_navigation.R
 import com.android_navigation.viewmodel.EstadoAppViewModel
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.main_activity.*
 import org.koin.android.viewmodel.ext.android.viewModel
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,6 +29,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+
+        val instanceFirebase = Firebase.auth
+        val task = instanceFirebase.createUserWithEmailAndPassword(
+            "gustavo.pereira.rocha@lojaesporte.com",
+            "123456"
+        )
+
+        task.addOnSuccessListener {
+            Toast.makeText(this, "Usuário foi cadastrado com sucesso", Toast.LENGTH_SHORT).show()
+        }
+        task.addOnFailureListener {
+            Log.e(TAG, "onCreate: ", it)
+            Toast.makeText(this, "Aconteceu uma falha ao cadastrar $it", Toast.LENGTH_SHORT).show()
+        }
+
+
+
         controller.addOnDestinationChangedListener { _, destination, _ ->
             title = destination.label
 
